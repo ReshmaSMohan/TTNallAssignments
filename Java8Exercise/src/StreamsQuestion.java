11eb34110178f60f98feaa72e08016dfc7b83f2d
ReshmaSMohan <reshma.mohan@tothenew.com>
2019-03-01 09:54:52 +0530

    streams edited

--- Java8Exercise/src/StreamsQuestion.java
+++ Java8Exercise/src/StreamsQuestion.java
@@ -5,11 +5,11 @@ import java.util.stream.Stream;
 
 public class StreamsQuestion {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> list = Arrays.asList(1,2,15,3,4,5,6,7,8,9,10);
 
         /*Collect all the even numbers from an integer list.*/
         System.out.println(list.stream()
-                .filter(val->val>5)
+                .filter(val->val%2==0)
                 .collect(Collectors.toList()));
 
         /*Sum all the numbers greater than 5 in the integer list.*/
@@ -23,10 +23,15 @@ public class StreamsQuestion {
                 .collect(Collectors.averagingInt(x -> x ))
 
         );
+        System.out.println(list.stream()
+                .collect(Collectors.averagingInt(x -> x*2 ))
+
+        );
 
         /*Find the first even number in the integer list which is greater than 3*/
         System.out.println(list.stream()
                 .filter(val -> val>3)
+                .filter(val->val%2==0)
                 .findFirst()
         );
     }
