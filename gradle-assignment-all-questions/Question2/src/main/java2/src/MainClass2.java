a499ea45e4a41a8d8b2706cd85654f1d27064159
ReshmaSMohan <reshma.mohan@tothenew.com>
2019-03-05 21:59:31 +0530

    Question2 added

---
+++ gradle-assignment-all-questions/Question2/src/main/java2/src/MainClass2.java
@@ -0,0 +1,12 @@
+public class MainClass2 {
+    {
+        System.out.println("java2");
+    }
+    public static void main(String[] args) {
+        System.out.println("java2");
+    }
+
+    public static void method1(){
+        System.out.println("java2 method");
+    }
+}
