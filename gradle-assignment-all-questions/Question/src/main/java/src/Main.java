e79c6e1337b1cdb1b3108592d2f9a64361c0c426
ReshmaSMohan <reshma.mohan@tothenew.com>
2019-03-06 09:26:03 +0530

    Q4 and Q6 added

---
+++ gradle-assignment-all-questions/Question/src/main/java/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Reshma S Mohan";
+        System.out.println(StringUtils.repeat('@',2));
+        System.out.println(StringUtils.swapCase(name));
+    }
+}
